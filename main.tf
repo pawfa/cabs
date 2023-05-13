d17b476572b3211290a0695a53d86cbf12f433e3
Pawel Fajfer <fajfer.pawel@gmail.com>
2023-05-13 20:13:38 +0200

    Created terraform configuration for EKS

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  cluster_name = "prod-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -9,11 +18,23 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/28"]
-  public_subnets  = ["10.0.101.0/28"]
+  private_subnets = ["10.0.1.0/28", "10.0.2.0/28"]
+  public_subnets  = ["10.0.3.0/28","10.0.4.0/28"]
 
   tags = {
     Terraform = "true"
     Environment = "prod"
   }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.5.1"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.24"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
 }
